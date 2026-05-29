8740f4030d815fdd51c628233f9f8173c41e2c16
Your Name <apatel7748@gmail.com>
2026-05-29 23:46:27 +0530

    tokan error fix

--- src/services/APIConnector.js
+++ src/services/APIConnector.js
@@ -3,11 +3,17 @@ import axios from "axios";
 export const axiosInstance = axios.create({});
 
 export const apiConnector = (method, url, bodyData, headers, params) => {
+    const token = typeof window !== 'undefined' ? localStorage.getItem('token') : null;
+    const mergedHeaders = { ...headers };
+    if (token && !mergedHeaders.Authorization && !mergedHeaders.authorization) {
+        mergedHeaders.Authorization = `Bearer ${token}`;
+    }
+
     return axiosInstance({
         method: `${method}`,
         url: `${url}`,
         data: bodyData ? bodyData : null,
-        headers: headers ? headers : null,
+        headers: Object.keys(mergedHeaders).length > 0 ? mergedHeaders : null,
         params: params ? params : null,
         withCredentials: true
     });
